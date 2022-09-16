6838925149b3cef4dbbf9d48d6a6ad8fa3ed5363
AugustoCRX <98962835+AugustoCRX@users.noreply.github.com>
2022-09-16 20:04:43 -0300

    python function to main file

---
+++ local_libs/color_filter.py
@@ -0,0 +1,15 @@
+#Filtro de cores para strings no python
+
+class bcolors:
+    MARGENTA = '\033[35m'
+    BLUE = "\033[34m"
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+if __name__ == '__main__':
+    colors = bcolors
